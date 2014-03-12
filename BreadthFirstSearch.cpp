a672c4425655bd4e517c4eea0aa45115f7132b14
luigi1015 <github@314ghz.com>
2014-03-11 19:30:38 -0500

    Got the BreadthFirstSearch to work.

--- BreadthFirstSearch.cpp
+++ BreadthFirstSearch.cpp
@@ -2,6 +2,7 @@
 #include "Edge.h"
 #include <vector>
 #include <queue>
+#include <iostream>
 
 enum BFSColors { BFSWhite, BFSGray, BFSBlack };
 
@@ -9,7 +10,7 @@ struct BreadthFirstTreeInfo
 {
 	long distance;//The distance (the minimum number of edges) from this vertex to the source vertex. -1 means infinite distance (in other words, disconnected from the tree).
 	long predecessor;//The predecessor of the vertex. -1 means there is no predecessor.
-}
+};
 
 template <class T> std::vector< Graph::Vertex<BreadthFirstTreeInfo> > breadthFirstSearch( std::vector< Graph::Vertex<T> > graph, size_t sourceVertexIndex )
 {//Does a breadth first search on graph starting at the vertex at index sourceVertexIndex of the vector. Returns the breadth-first tree. **Assumes the link IDs  in graph are the same as the Vector indices.**
@@ -18,6 +19,8 @@ template <class T> std::vector< Graph::Vertex<BreadthFirstTreeInfo> > breadthFir
 	std::vector<long> predecessors;//The predecessors of each vertex. -1 means there is no predecessor.
 	std::vector< Graph::Vertex<BreadthFirstTreeInfo> > breadthFirstTree;
 	std::queue< Graph::Vertex<T> > grayList;
+
+	std::cout << "Test 0" << std::endl;
 
 	for( int i = 0; i < graph.size(); i++ )
 	{
@@ -31,7 +34,7 @@ template <class T> std::vector< Graph::Vertex<BreadthFirstTreeInfo> > breadthFir
 			BreadthFirstTreeInfo sourceInfo;
 			sourceInfo.distance = 0;
 			sourceInfo.predecessor = -1;
-			Vertex<BreadthFirstTreeInfo> sourceVertex( i, sourceInfo );
+			Graph::Vertex<BreadthFirstTreeInfo> sourceVertex( i, sourceInfo );
 			breadthFirstTree.push_back( sourceVertex );
 		}
 		else
@@ -44,36 +47,57 @@ template <class T> std::vector< Graph::Vertex<BreadthFirstTreeInfo> > breadthFir
 			BreadthFirstTreeInfo sourceInfo;
 			sourceInfo.distance = -1;
 			sourceInfo.predecessor = -1;
-			Vertex<BreadthFirstTreeInfo> sourceVertex( i, sourceInfo );
+			Graph::Vertex<BreadthFirstTreeInfo> sourceVertex( i, sourceInfo );
 			breadthFirstTree.push_back( sourceVertex );
 		}
 	}
-		
+
+	std::cout << "Test 1" << std::endl;
+
 	grayList.push( graph.at(sourceVertexIndex) );
-		
+
+	std::cout << "Test 2" << std::endl;
+
 	while( !(grayList.empty()) )
 	{
 		Graph::Vertex<T> newVertex = grayList.front();
 		grayList.pop();
-			
+
+	std::cout << "Test 3" << std::endl;
+	std::cout << "ID: " << newVertex.getID() << " Num Edges: " << newVertex.getNumEdges() << std::endl;
+
+
 		for( int i = 0; i < newVertex.getNumEdges(); i++ )
 		{
+	std::cout << "Test 4, i=" << i << " End Vertex = " << newVertex.getEdge(i).getEndVertex() << std::endl;
+
 			if( colors.at(newVertex.getEdge(i).getEndVertex()) == BFSWhite )
 			{
+	std::cout << "Test 5" << std::endl;
+
 				colors.at(newVertex.getEdge(i).getEndVertex()) = BFSGray;
 				distances.at(newVertex.getEdge(i).getEndVertex()) = distances.at(newVertex.getID()) + 1;
 				predecessors.at(newVertex.getEdge(i).getEndVertex()) = newVertex.getID();
 				grayList.push( graph.at(newVertex.getEdge(i).getEndVertex()) );
-				
-				breadthFirstTree.at(newVertex.getEdge(i).getEndVertex()).getValue().distance = distances.at(newVertex.getEdge(i).getEndVertex());
-				breadthFirstTree.at(newVertex.getEdge(i).getEndVertex()).getValue().predecessor = predecessors.at(newVertex.getEdge(i).getEndVertex());
-				Edge newEdge( 1, newVertex.getID(), newVertex.getEdge(i).getEndVertex() );
+
+	std::cout << "Test 6" << std::endl;
+
+				BreadthFirstTreeInfo newBFSInfo;
+				newBFSInfo.distance = distances.at(newVertex.getEdge(i).getEndVertex());
+				newBFSInfo.predecessor = predecessors.at(newVertex.getEdge(i).getEndVertex());
+				breadthFirstTree.at(newVertex.getEdge(i).getEndVertex()).setValue( newBFSInfo );
+				//breadthFirstTree.at(newVertex.getEdge(i).getEndVertex()).getValue().distance = distances.at(newVertex.getEdge(i).getEndVertex());
+				//breadthFirstTree.at(newVertex.getEdge(i).getEndVertex()).getValue().predecessor = predecessors.at(newVertex.getEdge(i).getEndVertex());
+				Graph::Edge newEdge( 1, newVertex.getID(), newVertex.getEdge(i).getEndVertex() );
 				breadthFirstTree.at(newVertex.getEdge(i).getEndVertex()).addEdge( newEdge );
 			}
 		}
+
+	std::cout << "Test 7" << std::endl;
+
 		
 		colors.at( newVertex.getID() ) = BFSBlack;
 	}
 	
-	return grayList;
+	return breadthFirstTree;
 }
--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
-all: Vertex.o Edge.o
+all: Vertex.o Edge.o BreadthFirstSearch.o Test
 
 Vertex.o: Vertex.cpp
 	g++ -g -Wall -c Vertex.cpp
 
 Edge.o: Edge.h Edge.cpp
 	g++ -g -Wall -c Edge.cpp
+
+BreadthFirstSearch.o: BreadthFirstSearch.cpp
+	g++ -g -Wall -c BreadthFirstSearch.cpp
+
+Test: test.cpp Vertex.o Edge.o BreadthFirstSearch.o
+	g++ -g -Wall -o Test test.cpp Vertex.o Edge.o BreadthFirstSearch.o
 
 clean:
 	rm *.o *~ *.gch
