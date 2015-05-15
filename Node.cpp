3955f317d018c81b635a6cb6530ab2f371818b1c
luigi1015 <github@314ghz.com>
2015-05-14 21:28:17 -0500

    Started a node source file.

---
+++ Node.cpp
@@ -0,0 +1,23 @@
+/*
+#include <vector>
+#include <ostream>
+
+namespace Graph
+{
+	template <class Key, Value> class Node
+	{//A single edge of a graph.
+		private:
+			Key key;
+			Value value;
+			Node left, right;
+			int N;
+
+		public:
+			Node( Key newKey, Value newValue, int newN );//Constructor with values.
+			int size() const;//Return the size.
+			Value get( Key getKey );
+			void put( Key newKey, Value newValue );
+	};
+}
+*/
+#include "Node.h"
