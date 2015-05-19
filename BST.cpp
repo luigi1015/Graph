94e91a7236c9d30b94ddaf05bc47e3d32a06f9aa
luigi1015 <github@314ghz.com>
2015-05-19 10:23:39 -0500

    Added BST.cpp

---
+++ BST.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <ostream>
+/*
+namespace Graph
+{
+	template <class Key, Value> class BST
+	{//A Binary Search Tree.
+		private:
+			Node root;
+
+		public:
+			int size() const;//Return the size.
+			Value get( Key getKey );
+			void put( Key newKey, Value newValue );
+	};
+}
+*/
+
+namespace Graph
+{
+}
