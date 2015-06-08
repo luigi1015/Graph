bf903ff1d6751a6696147060223ee8627a3f08fd
Jeff <github@314ghz.com>
2015-06-08 05:10:33 -0500

    Copied the Node class to BSTNode since it's really specifically for a Binary Search Tree.

---
+++ BSTNode.cpp
@@ -0,0 +1,41 @@
+/*
+#include <vector>
+#include <ostream>
+
+namespace Graph
+{
+	template <class Key, Value> class Node
+	{//A single edge of a graph.
+		private:
+			Key key;
+			Value value;
+			Node left, right;
+			int N;
+
+		public:
+			int size() const;//Return the size.
+			Value get( Key getKey );
+			void put( Key newKey, Value newValue );
+	};
+}
+*/
+#include "Node.h"
+
+namespace Graph
+{
+	template<typename Key, typename Value> BSTNode<Key, Value>::BSTNode()
+	{
+	}
+
+	int BSTNode::size() const
+	{//Return the size.
+	}
+
+	template <typename Key, typename Value> Value BSTNode<Key, Value>::get( Key getKey )
+	{//Return the value of the node with a given key.
+	}
+
+	template <typename Key, typename Value> void put( Key newKey, Value newValue )
+	{//Create a node in the tree with thespecified key and value.
+	}
+}
---
+++ BSTNode.h
@@ -0,0 +1,26 @@
+#ifndef BSTNode_h
+#define BSTNode_h
+
+#include <vector>
+#include <ostream>
+
+namespace Graph
+{
+	template <typename Key, typename Value> class BSTNode
+	{//A single edge of a graph.
+		private:
+			Key key;
+			Value value;
+			BSTNode left, right;
+			int N;
+
+		public:
+			//Node( Key newKey, Value newValue, int newN );//Constructor with values.
+			BSTNode();
+			int size() const;//Return the size.
+			Value get( Key getKey );
+			void put( Key newKey, Value newValue );
+	};
+}
+
+#endif /* BSTNode_h */
