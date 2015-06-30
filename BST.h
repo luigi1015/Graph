0a62bbb04698b5377e3b9c5cd97ae1d72a083f15
luigi1015 <github@314ghz.com>
2015-06-30 11:26:57 -0500

    Added a couple method definitions for floor.

--- BST.h
+++ BST.h
@@ -14,6 +14,7 @@ namespace Graph
 			Value get( Node subtreeNode, Key getKey );//Returns the value of the node with a specified key that is within the subtree rooted at subtreeNode. Returns null if the node isn't found.
 			Node put( Node rootNode, Key newKey, Value newValue );
 			Node min( Node minNode );
+			Key floor( Node rootNode, Key key );
 
 		public:
 			int size() const;//Returns the size of the tree.
@@ -21,6 +22,7 @@ namespace Graph
 			Value get( Key getKey );//Returns the value of the node with a specified key.
 			void put( Key newKey, Value newValue );
 			Key min();
+			Key floor( Key key );
 	};
 }
 
